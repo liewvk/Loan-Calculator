f70b210c9d881266d66f16a35ee56529f1184427
liewvk <voonkiong@gamil.com>
2025-08-04 00:18:02 +0800

    Add project files.

---
+++ Loan Calculator/Form1.Designer.vb
@@ -0,0 +1,199 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnCalculate = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        lblMonthlyPayment = New Label()
+        lblTotalInterest = New Label()
+        lblTotalPayment = New Label()
+        Label4 = New Label()
+        txtLoanAmount = New TextBox()
+        txtInterestRate = New TextBox()
+        Label5 = New Label()
+        Label6 = New Label()
+        txtLoanTerm = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btnCalculate
+        ' 
+        btnCalculate.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        btnCalculate.Location = New Point(218, 352)
+        btnCalculate.Name = "btnCalculate"
+        btnCalculate.Size = New Size(274, 49)
+        btnCalculate.TabIndex = 0
+        btnCalculate.Text = "Calculate Loan"
+        btnCalculate.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(152, 199)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(164, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Monthly Payment"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(152, 243)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(125, 25)
+        Label2.TabIndex = 1
+        Label2.Text = "Total Interest"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label3.Location = New Point(152, 287)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(134, 25)
+        Label3.TabIndex = 1
+        Label3.Text = "Total Payment"
+        ' 
+        ' lblMonthlyPayment
+        ' 
+        lblMonthlyPayment.BackColor = Color.White
+        lblMonthlyPayment.BorderStyle = BorderStyle.FixedSingle
+        lblMonthlyPayment.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblMonthlyPayment.Location = New Point(353, 199)
+        lblMonthlyPayment.Name = "lblMonthlyPayment"
+        lblMonthlyPayment.Size = New Size(164, 25)
+        lblMonthlyPayment.TabIndex = 1
+        ' 
+        ' lblTotalInterest
+        ' 
+        lblTotalInterest.BackColor = Color.White
+        lblTotalInterest.BorderStyle = BorderStyle.FixedSingle
+        lblTotalInterest.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblTotalInterest.Location = New Point(353, 243)
+        lblTotalInterest.Name = "lblTotalInterest"
+        lblTotalInterest.Size = New Size(164, 25)
+        lblTotalInterest.TabIndex = 1
+        ' 
+        ' lblTotalPayment
+        ' 
+        lblTotalPayment.BackColor = Color.White
+        lblTotalPayment.BorderStyle = BorderStyle.FixedSingle
+        lblTotalPayment.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        lblTotalPayment.Location = New Point(353, 287)
+        lblTotalPayment.Name = "lblTotalPayment"
+        lblTotalPayment.Size = New Size(164, 25)
+        lblTotalPayment.TabIndex = 1
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label4.Location = New Point(152, 58)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(127, 25)
+        Label4.TabIndex = 1
+        Label4.Text = "Loan Amount"
+        ' 
+        ' txtLoanAmount
+        ' 
+        txtLoanAmount.Location = New Point(354, 59)
+        txtLoanAmount.Name = "txtLoanAmount"
+        txtLoanAmount.Size = New Size(172, 27)
+        txtLoanAmount.TabIndex = 2
+        ' 
+        ' txtInterestRate
+        ' 
+        txtInterestRate.Location = New Point(354, 94)
+        txtInterestRate.Name = "txtInterestRate"
+        txtInterestRate.Size = New Size(172, 27)
+        txtInterestRate.TabIndex = 2
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label5.Location = New Point(152, 93)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(78, 25)
+        Label5.TabIndex = 1
+        Label5.Text = "Interest"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label6.Location = New Point(152, 130)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(100, 25)
+        Label6.TabIndex = 1
+        Label6.Text = "Loan Term"
+        ' 
+        ' txtLoanTerm
+        ' 
+        txtLoanTerm.Location = New Point(354, 127)
+        txtLoanTerm.Name = "txtLoanTerm"
+        txtLoanTerm.Size = New Size(172, 27)
+        txtLoanTerm.TabIndex = 2
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtLoanTerm)
+        Controls.Add(txtInterestRate)
+        Controls.Add(txtLoanAmount)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(lblTotalPayment)
+        Controls.Add(lblTotalInterest)
+        Controls.Add(lblMonthlyPayment)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label1)
+        Controls.Add(btnCalculate)
+        Name = "Form1"
+        Text = "Loan Caculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblMonthlyPayment As Label
+    Friend WithEvents lblTotalInterest As Label
+    Friend WithEvents lblTotalPayment As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtLoanAmount As TextBox
+    Friend WithEvents txtInterestRate As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtLoanTerm As TextBox
+
+End Class
---
+++ Loan Calculator/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports Microsoft.VisualBasic
+Public Class Form1
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click, Label4.Click, Label5.Click, Label6.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim loanAmount As Double
+        Dim annualRate, monthlyRate As Double
+        Dim loanTermYears, loanTermMonths As Integer
+        Dim monthlyPayment As Double
+
+        ' Get user input
+        If Not Double.TryParse(txtLoanAmount.Text, loanAmount) Then
+            MessageBox.Show("Please enter a valid loan amount")
+            Return
+        End If
+
+        If Not Double.TryParse(txtInterestRate.Text, annualRate) Then
+            MessageBox.Show("Please enter a valid interest rate")
+            Return
+        End If
+
+        If Not Integer.TryParse(txtLoanTerm.Text, loanTermYears) Then
+            MessageBox.Show("Please enter a valid loan term")
+            Return
+        End If
+
+        ' Calculate monthly values
+        monthlyRate = annualRate / 100 / 12
+        loanTermMonths = loanTermYears * 12
+
+        ' Calculate payment using Financial.Pmt method
+        monthlyPayment = Pmt(monthlyRate, loanTermMonths, -loanAmount)
+
+        ' Display result
+        lblMonthlyPayment.Text = monthlyPayment.ToString("C")
+
+        ' Calculate and display amortization details
+        CalculateAmortization(loanAmount, monthlyRate, loanTermMonths, monthlyPayment)
+    End Sub
+
+    Private Sub CalculateAmortization(amount As Double, rate As Double,
+                                   periods As Integer, payment As Double)
+        Dim balance As Double = amount
+        Dim interest, principal As Double
+
+        ' Calculate total interest and principal
+        Dim totalInterest As Double = 0
+
+        For period As Integer = 1 To periods
+            interest = Math.Round(balance * rate, 2)
+            principal = Math.Round(payment - interest, 2)
+            balance = Math.Round(balance - principal, 2)
+            totalInterest += interest
+        Next
+
+        ' Display summary
+        lblTotalInterest.Text = totalInterest.ToString("C")
+        lblTotalPayment.Text = (amount + totalInterest).ToString("C")
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
